b1e173ca5ce88d39f492824d4ea9570574310194
SheldonMarshall <smarsha1@unca.edu>
2024-03-26 16:11:45 -0400

    Finished the trending functionality and deleted unwanted files

--- giphy_api.py
+++ giphy_api.py
@@ -4,17 +4,17 @@ import requests
 class GiphyAPI:
        
     #API Constructor
-    def __init__(self, api_key):
-        self.api_key = api_key
-        self.base_url = 'https://api.giphy.com/v1/gifs'
+    # def __init__(self, api_key):
+    #     self.api_key = api_key
+    #     self.base_url = 'https://api.giphy.com/v1/gifs'
     
-    def call_search(self,count, term):
-        endpoint = f"https://api.giphy.com/v1/gifs/search?api_key={self.api_key}&q={term}&limit={count}&offset=0&rating=g"
+    def call_search(self, API_Key, count, term):
+        endpoint = f"https://api.giphy.com/v1/gifs/search?api_key={API_Key}&q={term}&limit={count}&offset=0&rating=g"
         data = requests.get(endpoint)
         return data.json()
 
-    def call_trending(self,count):
-        endpoint = f"https://api.giphy.com/v1/gifs/trending?api_key={self.api_key}&limit={count}&offset=0&rating=g"
+    def call_trending(self, API_Key, count):
+        endpoint = f"https://api.giphy.com/v1/gifs/trending?api_key={API_Key}&limit={count}&offset=0&rating=g"
         data = requests.get(endpoint)
         return data.json()
     
